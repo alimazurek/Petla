5bd379bb338d0ee8b102a539bf6f6871fba8c3a9
alimazurek <99614006+alimazurek@users.noreply.github.com>
2022-03-08 19:34:34 +0100

    Initial commit

---
+++ src/Petla.java
@@ -0,0 +1,12 @@
+public class Petla {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<10) {
+            System.out.println("Liczę sobie");
+        }
+
+    }
+}
+/* dlaczego w tym przypadku wyskakuje mi milion Liczę sobie? Przeciez warunek spelnia sie tylko 10 ray tj od 0 do 9.
+ Kiedy dodam i++ dziala prawidlowo...
+ */
